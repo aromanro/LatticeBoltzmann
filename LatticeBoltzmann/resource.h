a8709127d97f0f967afa4c787bea3619e7d294d9
Adrian Roman <aroman_ro@yahoo.com>
2018-12-15 12:47:08 +0200

    Added links to GitHub repository and Blog description to About dialog

--- LatticeBoltzmann/resource.h
+++ LatticeBoltzmann/resource.h
@@ -44,7 +44,9 @@
 #define IDC_MFCCOLORBUTTON1             1007
 #define IDC_RADIO7                      1007
 #define IDC_EDIT3                       1008
+#define IDC_MFCLINK1                    1008
 #define IDC_EDIT4                       1009
+#define IDC_MFCLINK2                    1009
 #define IDC_EDIT5                       1010
 #define IDC_EDIT6                       1013
 #define IDC_EDIT7                       1014
@@ -54,9 +56,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        313
+#define _APS_NEXT_RESOURCE_VALUE        314
 #define _APS_NEXT_COMMAND_VALUE         32772
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
